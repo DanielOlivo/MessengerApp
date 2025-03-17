b813cb0d2ddb13424b6ff92ae3be84277f29d328
DanielOlivo <vit.masterov@gmail.com>
2025-03-17 17:40:59 +0200

    ChatView: init

---
+++ client/src/ChatView/ChatView.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Provider } from '../utils/Provider';
+import { ChatView } from './ChatView';
+
+const meta = {
+  title: 'ChatView/ChatView',
+  component: ChatView,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} satisfies Meta<typeof ChatView>;
+
+export default meta;
+type Story = StoryObj<typeof ChatView>;
+
+export const Primary: Story = {
+    decorators: [
+        (Story) => <Provider><Story /></Provider>
+    ]
+}
--- client/src/ChatView/ChatView.tsx
+++ client/src/ChatView/ChatView.tsx
@@ -1,5 +1,11 @@
+import { Header } from "./components/Header"
+import { Container } from "./components/Container"
+
 export const ChatView = () => {
   return (
-    <div>ChatView</div>
+    <div className="flex-grow flex flex-col justify-start items-stretch">
+      <Header />
+      <Container />
+    </div>
   )
 }
