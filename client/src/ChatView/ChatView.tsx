f78fda9c6062efc79fc8a790605b88e9981c455c
DanielOlivo <vit.masterov@gmail.com>
2025-03-18 13:11:29 +0200

    ChatView: + ChatInputt; Header padding

--- client/src/ChatView/ChatView.tsx
+++ client/src/ChatView/ChatView.tsx
@@ -1,11 +1,13 @@
 import { Header } from "./components/Header"
 import { Container } from "./components/Container"
+import { ChatInput } from "./components/ChatInput/ChatInput"
 
 export const ChatView = () => {
   return (
     <div className="h-full flex flex-col justify-start items-stretch">
       <Header />
       <Container />
+      <ChatInput />
     </div>
   )
 }
--- client/src/ChatView/components/Header.tsx
+++ client/src/ChatView/components/Header.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
     const {iconSrc, title} = useAppSelector(selectHeader)
 
     return (
-        <div className="flex flex-row justify-between items-center h-16">
+        <div className="flex flex-row justify-between items-center h-16 px-4 py-2">
 
             <div className="flex flex-row justify-start items-center">
                 <Icon iconSrc={iconSrc} isOnline={false}/>                
