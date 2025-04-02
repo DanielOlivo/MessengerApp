6f58677bae158f493d9094513cc9ed82c24f1006
DanielOlivo <vit.masterov@gmail.com>
2025-04-02 16:45:13 +0300

    features/state removed

--- client/src/features/state/selectors.ts
+++
@@ -1,7 +0,0 @@
-import { RootState } from "../../app/store";
-
-export const selectGlobalState = (state: RootState) => 
-    state.state.state
-
-export const selectActiveChat = (state: RootState) =>
-    state.state.activeChatId
