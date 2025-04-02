05fa61dca00c51c6622c6379c7070cf53b4a6103
DanielOlivo <vit.masterov@gmail.com>
2025-04-02 16:35:11 +0300

    features/chatlist removed

--- client/src/features/chatList/selectors.ts
+++
@@ -1,7 +0,0 @@
-import { RootState } from "../../app/store";
-
-export const selectListState = (state: RootState) => state.chatList.state
-
-export const selectChatList = (state: RootState) => state.chatList.list
-
-export const selectSearchResult = (state: RootState) => state.chatList.searchResult
