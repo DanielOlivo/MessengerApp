c7ea2ab737e95c619f90e3eb02746c23f6266831
DanielOlivo <vit.masterov@gmail.com>
2025-04-02 16:34:14 +0300

    getState: ununsed slices removed

--- client/src/utils/getState.ts
+++ client/src/utils/getState.ts
@@ -1,11 +1,5 @@
 import { RootState } from "../app/store";
-import { getChatItem } from "./getChatItem";
-import { getItems } from "./textMessageGen";
 import { ChatId, MessageId, UserId, UserInfo } from "../../../shared/src/Types";
-import { ContainerItem } from "../ChatPage/components/ChatView/slice";
-import { faker } from "@faker-js/faker";
-import { getRandomIcon } from "./getRandomIcon";
-import { Typings } from "../ChatPage/components/ChatView/components/Typing/utils";
 import { getRandomSliceState } from "../ChatPage/utils";
 import { ChatInfo } from "../ChatPage/slice";
 import { TextMessageProps } from "../ChatPage/components/ChatView/components/TextMessage/TextMessage";
@@ -19,9 +13,6 @@ export type DeepPartial<T> = T extends Array<infer U>
     : T
 
 export function getState(state?: DeepPartial<RootState>): RootState {
-
-    const msgs = getItems()
-    const currentChatId = Object.keys(msgs)[0]
 
     const chat = getRandomSliceState()
     const defaultGroup = getDefault()
@@ -52,60 +43,12 @@ export function getState(state?: DeepPartial<RootState>): RootState {
         auth: {
             authenticated: true,
         },
-        chatList: {
-            list: [],
-            state: 'list',
-            searchResult: [] 
-        },
-        chatView: {
-            userId: '',
-            chatId: '',
-            header: {
-                chatName: '',
-                count: 0
-            },
-            messages: []
-        },
-        header: {
-            typingTrigger: false
-        },
-        // group: {
-        //     isOn: false,
-        //     members: []
-        // },
         socket: {
             isConnected: false,
-            rooms: [],
-            msg: '',
-            user: {
-                id: '',
-                username: '',
-                hashed: '',
-                created: new Date(),
-            },
-            users: {},
-            dms: {},
-            groups: {},
-            memberships: {},
-            messages: {},
-            typing: [],
-            online: [],
-            overlayed: false
         },
-        state: {
-            state: 'idle'
-        },
-        chatList2: {
-            items: state?.chatList2?.items ?? Array.from({length: 20}, () => getChatItem())
-        },
-        chatView2: {
-            items: (state?.chatView2?.items as {[P in ChatId]: ContainerItem[]} | undefined) ?? msgs,
-            chatId: state?.chatView2?.chatId ?? currentChatId,
-            header: {
-                title: state?.chatView2?.header?.title ?? faker.person.fullName(),
-                iconSrc: state?.chatView2?.header?.iconSrc ?? getRandomIcon().iconSrc
-            },
-            typing: state?.chatView2?.typing as Typings ?? {}
+        search: {
+            onSearch: state?.search?.onSearch ?? false,
+            result: state?.search?.result ?? [] 
         }
     }
 
