a5d45f5638e35605a834ce4ddcf6f323df72bff4
DanielOlivo <vit.masterov@gmail.com>
2025-04-03 13:26:02 +0300

    ChatItem spec: click changes slice state

--- client/src/ChatPage/components/ChatList/components/ChatItem.spec.tsx
+++ client/src/ChatPage/components/ChatList/components/ChatItem.spec.tsx
@@ -1,8 +1,11 @@
 import { describe, test, expect } from "vitest";
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import { ChatItem } from "./ChatItem";
 import { Provider } from "../../../../utils/Provider";
+import { Provider as RProvider } from "react-redux";
 import { getRandomChatItem } from "../utils";
+import { getState } from "../../../../utils/getState";
+import { createStore } from "../../../../app/store";
 
 describe('ChatItem', () => {
 
@@ -13,5 +16,14 @@ describe('ChatItem', () => {
 
         expect(screen.queryByText(new RegExp(props.title))).toBeInTheDocument()
         expect(screen.queryByText(new RegExp(props.content))).toBeInTheDocument()
+    })
+
+    test('click opens the chat', async () => {
+        const props = getRandomChatItem()
+        const store = createStore(getState())
+
+        render(<RProvider store={store}><ChatItem {...props} /></RProvider>)
+
+        await waitFor(() => expect(store.getState().chat.displayedChatId).toEqual(props.chatId))
     })
 })
