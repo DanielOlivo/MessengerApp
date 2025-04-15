397bd1f4d78d85dcbaa253c28455c3a182391e5f
DanielOlivo <vit.masterov@gmail.com>
2025-04-15 17:37:41 +0300

    removing unused

--- client/src/ChatPage/components/ChatList/components/NewGroupButton.tsx
+++ client/src/ChatPage/components/ChatList/components/NewGroupButton.tsx
@@ -1,13 +1,9 @@
-import { createPortal } from "react-dom"
-import { useApDispatch, useAppSelector } from "../../../../app/hooks"
-import { selectState } from "../../../../ChatControl/selectors"
-import { CreateWindow } from "../../../../ChatControl/components/CreateWindow"
+import { useApDispatch } from "../../../../app/hooks"
 import { setCreate } from "../../../../ChatControl/slice"
 
 export const NewGroupButton = () => {
 
     const dispatch = useApDispatch()
-    const state = useAppSelector(selectState)
 
     return (
         <div className="flex flex-row justify-center items-center h-9 my-2">
@@ -16,12 +12,6 @@ export const NewGroupButton = () => {
                 onClick={() => dispatch(setCreate())}
                 aria-label="new-group-btn"
             >New Group</button>
-
-            {state === 'onCreate' && createPortal(
-                <div className="w-full h-full bg-[rgba(200,200,200, 0.3)] flex justify-center items-center">
-                    <CreateWindow /> 
-                </div>
-            , document.body)}
         </div>
     )
 }
