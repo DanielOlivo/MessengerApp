09288e45c66c63787f5cd5e958848d02e37a960b
DanielOlivo <vit.masterov@gmail.com>
2025-04-20 09:46:44 +0300

    removed outdated model scripts

--- server/src/models/search.ts
+++
@@ -1,9 +0,0 @@
-import db from '@config/db'
-
-export default {
-    search
-}
-
-export function search (criteria: string) {
-    return db('users').whereLike('username', '%' + criteria + '%').select('id', 'username')
-}
