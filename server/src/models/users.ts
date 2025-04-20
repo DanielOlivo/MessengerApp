54357645f335c65c699c8339093d9b41d0ff1511
DanielOlivo <vit.masterov@gmail.com>
2025-04-20 08:44:01 +0300

    models: User

--- server/src/models/users.ts
+++ server/src/models/users.ts
@@ -1,68 +1,40 @@
-import { DbUser, UserId } from '@shared/Types';
+import { UserId } from '@shared/Types';
+import { User } from './models';
 import db from '../config/db'
-import {v4 as uuid4} from 'uuid'
 
 const model = {
-    create: async(username: string, hashed: string, bio?: string) => {
-        const [user]: DbUser[] = await db('users')
-            .insert({username, hashed, bio}, 
-            //    ['id', 'bio', 'username', 'hashed', 'created'] 
-                ['*']
-            )
-            return user;
+    create: async(username: string, hashed: string, bio?: string): Promise<User> => {
+        const [user]: User[] = await db('users')
+            .insert({username, hashed, bio}, ['*'])
+        return user;
     },
 
-    // create: async(username: string, hashed: string, bio?:string) =>{
-    //     const result: unknown = await db.transaction(async trx => {
-    //         return trx('users').insert({username, hashed, bio}).returning('*')
-    //     })
-    //     const [user] = result as DbUser[]
-    //     return user
-    // },
-
-    remove: async(userId: UserId) => {
-        const [{id}]: Partial<DbUser>[] = await db('users')
-            .where('id', userId)
-            .del(['id'])
+    remove: async(userId: UserId): Promise<UserId> => {
+        const [{id}]: Partial<User>[] = await db('users').where('id', userId).del(['id'])
         return id as UserId
     },
 
-    getAll: async() => {
-        const all: DbUser[] = await db('users').select('*')
+    getAll: async(): Promise<User[]> => {
+        const all: User[] = await db('users').select('*')
         return all
     },
 
-    getByUsername: async(username: string) => {
-        const user = await db('users').where({username}).first() as DbUser
+    getByUsername: async(username: string): Promise<User[]> => {
+        const user = await db('users').where({username}).select('*')
         return user
     },
 
-    getById: async(userId: UserId) => {
-        const user = await db('users')
-            .where('id', userId)
-            .select('*')
-            .first() as DbUser
-        return user 
+    getById: async(id: UserId): Promise<User[]> => {
+        const users = await db('users').where({id}).select('*')
+        return users 
     },
 
-    searchByUsername: async(username: string) => {
+    searchByUsername: async(username: string): Promise<User[]> => {
         const users = await db('users')
             .whereRaw('LOWER(username) LIKE LOWER( ? )', ["%".concat(username, '%')])
-            .select('*') as DbUser[]
+            .select('*')
         return users
     },
-
-    handleSearchBy: async(id: UserId, criteria: string) => {
-        const result = await db('users')
-            .whereRaw("LOWER(username) LIKE LOWER(?)", ['%' + criteria.toLowerCase() + '%'])
-            .andWhereNot({id})
-            .select('id', 'username', 'created', 'bio', ) as Omit<DbUser, 'hashed'>[]
-        return result
-    }
-
 }
 
 export default model
-
-
-// export type {UserModel}
